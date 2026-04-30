a786bbf85cfb6bdd0aafa396ed2a55599ca9f573
handshou <handshou@users.noreply.github.com>
2026-04-30 14:08:15 +0800

    Add git alias scripts: setup-work, clone-bare-for-worktrees

---
+++ scripts/git-setup-work.sh
@@ -0,0 +1,5 @@
+git config user.name hanselse
+git config user.email 190053598+hanselse@users.noreply.github.com
+git config gpg.format ssh
+git config user.signingkey ~/.ssh/id_ed25519_work.pub
+git config commit.gpgsign true
